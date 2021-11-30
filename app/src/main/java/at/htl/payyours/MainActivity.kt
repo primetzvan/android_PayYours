80a86ac665f10212055fabf202d89d1d09093ec3
primetzvan <vaniprimetz@gmail.com>
2021-11-30 17:11:17 +0100

    update 30.11 17:11

--- app/src/main/java/at/htl/payyours/MainActivity.kt
+++ app/src/main/java/at/htl/payyours/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -12,6 +13,10 @@ import androidx.activity.result.contract.ActivityResultContracts
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+  companion object {
+    private val PAYMENT_KEY = "PAY_YOURS_PAYMENT"
+  }
 
   var payment: Payment = Payment(0, 0, 0.0)
 
@@ -24,9 +29,24 @@ class MainActivity : AppCompatActivity() {
     btn_player_plus.setOnClickListener { add(edit_player_main, 1) }
     btn_minus_player.setOnClickListener { add(edit_player_main, -1) }
 
-    payment = SettingsActivity.getStoredPayment(this)
+    if(savedInstanceState == null){
+      payment = SettingsActivity.getStoredPayment(this)
+    }else{
+      savedInstanceState.get(PAYMENT_KEY) as Payment
+    }
+
     updatePaymentTextView()
 
+  }
+
+  override fun onSaveInstanceState(outState: Bundle) {
+    outState.putSerializable(PAYMENT_KEY,payment)
+    super.onSaveInstanceState(outState)
+  }
+
+  override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+    super.onRestoreInstanceState(savedInstanceState)
+    payment = savedInstanceState.getSerializable(PAYMENT_KEY) as Payment
   }
 
   override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -69,14 +89,18 @@ class MainActivity : AppCompatActivity() {
     edit_places_main.setText("${payment.courts}")
     edit_player_main.setText("${payment.players}")
     resultView.setText(String.format("%.2f", payment.amount))
+    btn_minus_player.isEnabled = payment.players > 1
+    btn_minus_place.isEnabled = payment.courts > 1
 
   }
 
   private fun add(textView: TextView, value: Int) {
     val newValue = textView.text.toString().toInt() + value
-    textView.text = "$newValue"
-    updatePayment()
-    updatePaymentTextView()
+    if(newValue >= 0) {
+      textView.text = "$newValue"
+      updatePayment()
+      updatePaymentTextView()
+    }
   }
 
   private fun updatePayment() {
